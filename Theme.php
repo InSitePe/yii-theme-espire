340d056fca8e584394c80c58cda8dcf979966753
Renziito <renzo.aliaga@upch.pe>
2017-10-17 10:45:19 -0500

    theme

--- Theme.php
+++ Theme.php
@@ -7,10 +7,19 @@
  */
 class Theme extends CThemeManager {
 
+    const DEFAULT_THEMEPATH = 'yii-theme-espire';
+
     public function init() {
-        $route = Yii::getPathOfAlias('webroot') . '/vendor/insite/yii-theme-espire';
-        $this->setBasePath($route);
-        parent::init();
+        $this->setBaseUrl($this->getUrlCore() . '/' . self::DEFAULT_THEMEPATH);
+        $this->setBasePath($this->getPathCore() . DIRECTORY_SEPARATOR . self::DEFAULT_THEMEPATH);
+    }
+
+    private function getPathCore() {
+        return realpath(dirname(__DIR__) . '/../');
+    }
+
+    private function getUrlCore() {
+        return Yii::app()->getBaseUrl(true);
     }
 
 }
